29a6bfdd674c96a474c5e351aa2b2375f3fd6190
Anna Sokolova <iambirdann@yandex.ru>
2022-05-02 12:19:21 +0300

    Added praktikum classes

---
+++ src/main/java/praktikum/UserData.java
@@ -0,0 +1,45 @@
+package praktikum;
+
+public class UserData {
+    private String name;
+    private String email;
+    private String password;
+
+    public UserData() {
+    }
+
+    public UserData(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public UserData(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
